d51128dfee53ae3fd51a081c8c85d62f15ff140a
Omar al Risi <66246757+Omar-Risi@users.noreply.github.com>
2026-04-27 18:39:52 +0400

    fix: removed circular reference

--- src/main/java/com/inventory/inventory_management/config/SecurityConfig.java
+++ src/main/java/com/inventory/inventory_management/config/SecurityConfig.java
@@ -36,14 +36,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
 }
